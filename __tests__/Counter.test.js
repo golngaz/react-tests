05189167ad2912d9ec91ec4a04f32e7628a2c8b1
galcantud <galcantud@b2pweb.com>
2019-07-02 15:30:25 +0200

    j'ai enfin trouvé commen bien configurer les tests proprement

--- __tests__/Counter.test.js
+++ __tests__/Counter.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import ReactTestUtils from 'react-dom/test-utils';
-import Counter from "./tmp/Counter";
+import Counter from "../src/Counter";
 
 let container;
 
