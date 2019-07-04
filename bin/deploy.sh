3e32160d36a75b84f1d4c9654f1e7d44ee3b9d53
galcantud <galcantud@b2pweb.com>
2019-07-04 14:27:21 +0200

    amélioration des scripts

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-rm -r dist && /opt/node-v10.16.0-linux-x64/bin/node node_modules/.bin/parcel build index.js && mv dist/* public
+
+cd dist
+ls -a . | grep ^[a-zA-Z] | xargs rm
+
+cd ..
+/opt/node-v10.16.0-linux-x64/bin/node node_modules/.bin/parcel build index.js && mv dist/* public
